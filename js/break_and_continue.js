ffd254b3035407bc81875ce166f0f9dc878da054
Riley King <rileyking646@gmail.com>
2022-09-19 10:59:49 -0400

    Picking the odd number is done

---
+++ js/break_and_continue.js
@@ -0,0 +1,15 @@
+// Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+// Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+/*
+let userNum = prompt("Please pick an odd number between 1 and 50")
+
+for (let i = 0; i <= 50; i++) {
+    if (i === parseInt(userNum)) {
+        continue;
+    }
+     else if (userNum % 2 === 0) {
+            alert("Please enter and odd number")
+            break;
+    }
+    console.log(i)
+}*/
