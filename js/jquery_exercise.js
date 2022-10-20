5551e37f02a3c5362460855661c46866026d6803
RileyK646 <RileyKing646@gmail.com>
2022-10-20 16:49:26 -0400

    selectors exercise

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -1,3 +1,12 @@
-$(function() {
-    alert( '#Hello' );
+/*$(function() {
+    alert( 'The DOM has finished loading!' );
+});*/
+/*
+alert($('#test'))*/
+
+$('.codeup').css( {
+    'border' : '1px solid red'
 });
+$('li').css('font-size', '20px')
+$('h1, p, li').css('background-color', 'lime')
+alert($('h1'))
