089752089c2b73f8ba469ecb824e2d7a8aa54a1a
RileyK646 <RileyKing646@gmail.com>
2022-10-27 11:04:12 -0400

    added js variables:

---
+++ js/weathermapYT.js
@@ -0,0 +1,9 @@
+const timeE = document.getElementById('time')
+const dateE = document.getElementById('date')
+const currentWeatherItemsE = document.getElementById("current-weather-items")
+const timezone = document.getElementById("time-zone")
+const countryE = document.getElementById('country')
+const weatherForcastE = document.getElementById("weather-forecast")
+const currentTempE = document.getElementById('current-temp')
+
+
