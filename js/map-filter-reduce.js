2acc6b95ff48f379971479db24c91468e15168f2
RileyK646 <RileyKing646@gmail.com>
2022-10-31 17:59:30 -0400

    reduce reuse and code

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,50 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+let ThreeLang = users.filter((e) => e.languages.length >= 3)
+console.log(ThreeLang)
+
+let emails = users.map(function (ele) {
+    return  ele.email
+})
+console.log(emails);
+
+let totalyears = users.reduce((total, person) => {
+    return total + person.yearsOfExperience
+}, 0)
+console.log(totalyears);
--- js/weathermapYT.js
+++ js/weathermapYT.js
@@ -6,4 +6,32 @@ const countryE = document.getElementById('country')
 const weatherForcastE = document.getElementById("weather-forecast")
 const currentTempE = document.getElementById('current-temp')
 
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept' , 'Oct', 'Nov', 'Dec'];
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth()
+    const date = time.getDate()
+    const day = time.getDay()
+    const hour = time.getHours()
+    const minutes = time.getMinutes()
+    const hours24 = hour >= 113 ? hour % 12 : hour
+    const ampm = hour >= 12 ? 'PM' : 'AM'
 
+        dateE.innerHTML =  days[day] + ', ' + date + ' ' + months[month]
+    timeE.innerHTML = hours24 + ":" + minutes + " " + `<span id="am-pm">${ampm}</span>`
+}, 1000);
+function getWeather() {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success)
+
+        let {latitude, longitude} = success.coords;
+        fetch(`api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${openweatherKey}`).then(res => res.json()).then(data => {
+            console.log(data)
+        })
+    })
+}
+getWeather()
+function showWeatherData(data) {
+
+}
